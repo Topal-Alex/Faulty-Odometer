1e7ae627250a606a18654798e8060239288587aa
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-15 22:34:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FaultyOdometer(55));
+
+        }
+
+        static public int FaultyOdometer(int n)
+        {
+            int NormalOdometry = 0;
+            string m = n.ToString();
+            int dlina = m.Length;
+            int[] digitArray = new int[dlina];
+            for (int k = 0; k < m.Length; k++)
+            {
+                digitArray[k] = int.Parse(m[k].ToString());
+            }
+            //int[] digitArray = m.Select(digit => int.Parse(digit.ToString())).ToArray();  short way 
+            Array.Reverse(digitArray);
+            for (int i = 0; i < digitArray.Length; i++)
+            {
+                if (digitArray[i] > 4)
+                {
+                    NormalOdometry += (int) ((digitArray[i] - 1) * System.Math.Pow(9, i));
+                }
+                else
+                {
+                    NormalOdometry += (int) (digitArray[i] * System.Math.Pow(9, i));
+                }
+                
+
+            }
+
+            return NormalOdometry;
+
+
+        }
+    }
+}
